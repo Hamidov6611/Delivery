87f6451084553decfdc347587d1bc1f6594de388
Hamidov6611 <dilmuroddelitot@gmail.com>
2023-09-09 10:47:17 +0500

    Delivery

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "72a6001a76761f31"; }
+/******/ 		__webpack_require__.h = function() { return "22ee80b367f1ff23"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
